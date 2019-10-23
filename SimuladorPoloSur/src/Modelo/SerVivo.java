600c9e81c0fb3a5046298075df7ca7b29505b149
raulg <raulg@LAPTOP-VH1MHHU0>
2019-10-23 12:46:16 +0200

    Creacion de paquetes y clases

---
+++ SimuladorPoloSur/src/Modelo/SerVivo.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author raulg
+ */
+public abstract class SerVivo {
+    
+    
+    private int dia_nac;
+    
+    private int por_masmusc;
+    
+    public SerVivo(){}
+    
+    public SerVivo(int dia_nac, int por_masmusc){
+        this.dia_nac = dia_nac;
+        this.por_masmusc = por_masmusc;
+    }
+    
+    public void comer(){
+        
+    }
+    
+    public void reproducirse(){
+        
+    }
+    
+    public void morir(){
+        
+    }
+    
+}
